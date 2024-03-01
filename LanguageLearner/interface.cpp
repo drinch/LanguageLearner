02661ce3ee3d7f00b62cf5d25475a63f369fca03
drinch <1250307437@qq.com>
2024-03-01 16:20:51 +0800

    考核窗口初步设计
    初步设计了考核窗口的工作机制，考核类型，界面

--- LanguageLearner/interface.cpp
+++ LanguageLearner/interface.cpp
@@ -1,11 +1,15 @@
 #include "interface.h"
 #include "ui_interface.h"
 
-Interface::Interface(QWidget *parent) :
+Interface::Interface(QWidget *parent):
 	QWidget(parent),
 	ui(new Ui::Interface)
 {
 	ui->setupUi(this);
+
+    connect(ui->TestA_,&QPushButton::click,[=](){
+        examine(1,ui->spinBox->value());
+    });
 }
 
 Interface::~Interface()
